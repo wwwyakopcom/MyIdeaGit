d34b36d8bda4341d6994e01b3cec75b3dd5bd4c3
yq <157468995@qq.com>
2019-03-29 14:20:04 +0800

    常è§创建了一个项目...

---
+++ src/main/java/com/yanqun/servlet/MyServlet.java
@@ -0,0 +1,29 @@
+package com.yanqun.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/MyServlet")
+public class MyServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("do post");
+        int a = 10 ;
+        int b = 20 ;
+        a = a + b ;
+        System.out.println(a);
+        System.out.println(b);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("do post");
+        int a = 10 ;
+        int b = 20 ;
+        a = a + b ;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
